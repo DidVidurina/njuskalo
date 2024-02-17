e593f2196374cffda5e57ad3db09f867d641c709
DidVidurina <113940916+DidVidurina@users.noreply.github.com>
2024-02-17 14:21:14 +0000

    Update html_pretraga.py

--- html_pretraga.py
+++ html_pretraga.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-print("tes1t")
+print("tes11t")
 print("test3")
 
 URL = 'https://www.njuskalo.hr/prodaja-kuca/bjelovar'
